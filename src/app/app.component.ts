2c9ed6d0bef2ea408c2eebc5efb28279cebb4562
SharonNavarro <shanmowrk@gmail.com>
2022-04-26 00:34:42 -0500

    style: login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'my-store';
+
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  }
+
+  onRegister() {
+    console.log(this.register)
+  }
+
 }
