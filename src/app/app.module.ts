8a6cb1508130a8321cc45ee7eca682e360a12ad4
SharonNavarro <shanmowrk@gmail.com>
2022-05-13 18:34:28 -0500

    add: images. style: nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
-import { NavComponent } from './component/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImgComponent,
     ProductComponent,
-    ProductsComponent,
-    NavComponent
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
