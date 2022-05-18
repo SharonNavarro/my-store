3fc94b52a14fd84f4b1292c9e26920871cf2a572
SharonNavarro <shanmowrk@gmail.com>
2022-05-17 23:02:50 -0500

    feat: show data from fakestore api

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model'
 import { StoreService } from 'src/app/services/store.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -9,46 +10,53 @@ import { StoreService } from 'src/app/services/store.service';
 })
 export class ProductsComponent implements OnInit {
 
+  myShoppingCart: Product[] = [];
+  total = 0;
+  products: Product[] = [];
+  today = new Date();
+  date = new Date(2021, 1, 21);
+
   constructor(
-    private storeService: StoreService
+    private storeService: StoreService,
+    private productsService: ProductsService
   ) {
     //lo ponemos aqui ya que no es asincrono
     this.myShoppingCart = this.storeService.getShoppingCart();
   }
 
   ngOnInit(): void {
+    this.productsService.getAllProducts()
+    .subscribe(data => {
+      this.products = data;
+    })
   }
 
-  myShoppingCart: Product[] = [];
-
-  total = 0;
-
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: '../assets/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: '../assets/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: '../assets/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: '../assets/books.jpg'
-    },
-  ];
+  // products: Product[] = [
+  //   {
+  //     id: '1',
+  //     name: 'EL mejor juguete',
+  //     price: 565,
+  //     image: '../assets/toy.jpg'
+  //   },
+  //   {
+  //     id: '2',
+  //     name: 'Bicicleta casi nueva',
+  //     price: 356,
+  //     image: '../assets/bike.jpg'
+  //   },
+  //   {
+  //     id: '3',
+  //     name: 'Colleción de albumnes',
+  //     price: 34,
+  //     image: '../assets/album.jpg'
+  //   },
+  //   {
+  //     id: '4',
+  //     name: 'Mis libros',
+  //     price: 23,
+  //     image: '../assets/books.jpg'
+  //   },
+  // ];
   // https://picsum.photos/200
 
   onAddToShppingCart(product: Product) {
