ec6343095ad0cad9ba7c85b86545e1d19ce363b1
SharonNavarro <shanmowrk@gmail.com>
2022-05-24 11:30:12 -0500

    add: auth service endpoints

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,5 +9,19 @@ import { User, CreateUsertDTO } from './../models/user.model'
 })
 export class AuthService {
 
-  constructor() { }
+  // Hay un proxy de desarrollo para evitar problemas de CORDS
+  private apiUrl = `${environment.API_URL}/api/auth`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(email: string, password: string) {
+    return this.http.post(`${this.apiUrl}/login`, {email, password});
+  };
+
+  profile(token: string) {
+    return this.http.get(`${this.apiUrl}/profile`);
+  };
+
 }
