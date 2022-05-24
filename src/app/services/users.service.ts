0a60697148e348ae8a533a1b5677194eef67c899
SharonNavarro <shanmowrk@gmail.com>
2022-05-24 11:15:11 -0500

    add: user model

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  // Hay un proxy de desarrollo para evitar problemas de CORDS
+  private apiUrl = `${environment.API_URL}/api/products`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
 }
